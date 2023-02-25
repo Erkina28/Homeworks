096cbb46eb4ed5e91b449b778ce4bd0a568f104f
Erkina28 <125510991+Erkina28@users.noreply.github.com>
2023-02-25 23:03:44 +0300

    Add files via upload

---
+++ DZ3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+double x, ostatok;
+Console.WriteLine("Введите значение x ");
+x = Convert.ToInt32(Console.ReadLine());
+ostatok = x % 2;
+if (ostatok == 0)
+    Console.WriteLine("Четное число");
+    else if (ostatok >= 1)
+    Console.WriteLine("Нечетное число");
+    
