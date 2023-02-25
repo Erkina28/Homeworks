760f777600e3f27af59eefeaecae9295737802d9
Erkina28 <125510991+Erkina28@users.noreply.github.com>
2023-02-25 23:04:18 +0300

    Add files via upload

---
+++ DZ4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+double N;
+Console.WriteLine("Введите значение N ");
+N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+ {
+    if (i%2==0)
+  {
+    Console.Write(i+1);
+}
+    Console.Write("{0} ", ++i);
+}
+           
